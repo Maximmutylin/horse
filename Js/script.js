827c57e2d9c75bd4f8b5f023eb6583986ec5078f
Maximmutylin <87900810+Maximmutylin@users.noreply.github.com>
2022-02-26 21:07:11 +0300

    ready site

---
+++ Js/script.js
@@ -0,0 +1,108 @@
+'use strict'
+
+//Меню
+const menu = document.querySelector('.header__menu');
+const fullMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.menu__close');
+
+
+menu.addEventListener('click', function(){
+    fullMenu.setAttribute('style', 'display: block;')
+})
+
+
+closeMenu.addEventListener('click', function(){
+    fullMenu.setAttribute('style', 'display: none;')
+})
+
+//Слайдер 1
+$('.slider123').slick({
+    dots: true,
+    waitForAnimate: true,
+    slidesToShow: 4,
+    slidesToScroll:4,
+    
+    responsive:[
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow:4,
+                slidesToScroll:4,
+            }
+        },
+
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow:3,
+                slidesToScroll:3,
+            }
+        },
+
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow:2,
+                slidesToScroll:2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow:1,
+                slidesToScroll:1,
+            }
+        }, 
+    ]
+});
+
+
+
+//Слайдер 2
+$('.horse__slaider-line').slick({
+    dots: true,
+    waitForAnimate: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive:
+    [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow:2,
+                slidesToScroll:1,
+            }
+        },
+        {
+            breakpoint: 770,
+            settings: {
+                slidesToShow:1,
+                slidesToScroll:1,
+            }
+        },
+    ]
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
